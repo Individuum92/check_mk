a1d60d3f85376fba44ba0cb5ebec3ebb48546126
Individuum92 <18618181+Individuum92@users.noreply.github.com>
2025-02-06 09:04:32 +0100

    Prefix hinzugefügt

--- Linux/check_folder_size.sh
+++ Linux/check_folder_size.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 # Ordnerpfad anpassen
-FOLDER="/var/"
+FOLDER="/path/"
 
-# Berechne die Größe des Ordners in GB (2 Nachkommastellen)
+# Toggle fuer Prefix
+PREFIX_ENABLED=1  # 1 = Prefix "Serancon: " aktiviert, 0 = Prefix deaktiviert
+
+# Berechne die Groesse des Ordners in GB (2 Nachkommastellen)
 SIZE_GB=$(du -s "$FOLDER" | awk '{printf "%.2f", $1 / 1024 / 1024}')
 
-# Status immer 0 (OK), da keine Warnungen/Kritischen Werte benötigt werden
+# Status immer 0 (OK), da keine Warnungen/Kritischen Werte benoetigt werden
 STATUS=0
-SERVICE_NAME="Folder Size of $FOLDER"
+BASE_SERVICE_NAME="Folder Size of $FOLDER"
+
+# Prefix-Logik
+if [ "$PREFIX_ENABLED" -eq 1 ]; then
+    SERVICE_NAME="Serancon: $BASE_SERVICE_NAME"
+else
+    SERVICE_NAME="$BASE_SERVICE_NAME"
+fi
 
 # Ausgabe im CheckMK-Format
-echo "$STATUS \"$SERVICE_NAME\" size=$SIZE_GB GB - Folder size: $SIZE_GB GB"
+echo "$STATUS \"$SERVICE_NAME\" size=$SIZE_GB Folder size: $SIZE_GB GB"
